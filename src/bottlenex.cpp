d5a08311fe1807badafe6814faf3b02d9b6e2af9
Arnaud Vericel <arnaud.vericel@gmail.com>
2022-12-01 20:55:34 +0100

    Cleaned the main function

--- src/bottlenex.cpp
+++ src/bottlenex.cpp
@@ -1,26 +1,12 @@
 #include <iostream>
-#include <vector>
-#include "../include/lane.h"
+#include "../include/road.h"
 #include "../include/car.h"
-
-using namespace std;
+#include "../include/lane.h"
 
 int main(int argc, char** argv)
 {
-    Lane lane_example = Lane(110., 3.);
 
-    Vehicle* new_car = new Car();
-    Vehicle* another_car = new Car();
-
-    lane_example.InsertVehicle(new_car);
-    lane_example.InsertVehicle(another_car);
-
-    vector<Vehicle*> vehiclesOnLane = lane_example.GetVehiclesOnLane();
-
-    for (Vehicle* vehicle : vehiclesOnLane)
-    {
-        vehicle->Move();
-    }
+    // Todo
 
     return 0;
 }
