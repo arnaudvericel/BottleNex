d7feaf9e0967f18492368006c15026ad459324aa
Arnaud Vericel <arnaud.vericel@gmail.com>
2022-12-05 20:25:23 +0100

    - Added new fields to better configurate the simulation

--- include/constants.h
+++ include/constants.h
@@ -3,22 +3,26 @@
 
 #include "enums.h"
 
+// fixme : all these constants must go to a config object at some point
+
 namespace constants
 {
     namespace sim
     {
-        constexpr int nbDumps = 1000; // fixme -> in a config object
-        constexpr float deltaTime = 0.1; // fixme -> in a config object
-        constexpr float maxTime = 600.; // fixme -> in a config object
+        constexpr int nbDumps = 1000;
+        constexpr float deltaTime = 0.1; // fixme : needs to be computed at each timestep with CFL condition
+        constexpr float maxTime = 600.;
+        constexpr float vehiclesPerMin = 30.;
     }
 
     namespace car
     {
-        constexpr float accelDistanceFactor = 5.;
-        constexpr float cruiseDistanceFactor = 10.;
+        constexpr float brakingDistanceFactor = 2.5;
+        constexpr float cruiseDistanceFactor = 5.;
+        constexpr float accelDistanceFactor = 7.5;
         constexpr Motion startMotion = Motion::Accelerating;
         constexpr float startVelocityFactor = 0.5;
-        constexpr float safeDistanceToEnterLaneFactor = 10.;
+        constexpr float safeDistanceToEnterLaneFactor = 2.;
     }
 }
 
