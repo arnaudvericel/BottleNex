2ff45e48069c2e1b19b476f8bb1ef095dfa998ba
Arnaud Vericel <arnaud.vericel@gmail.com>
2022-12-06 16:04:46 +0100

    - Modified the constants and added some

--- include/constants.h
+++ include/constants.h
@@ -7,22 +7,26 @@
 
 namespace constants
 {
+    namespace files
+    {
+        const std::string extension = ".dat";
+    }
     namespace sim
     {
-        constexpr int nbDumps = 1000;
-        constexpr float deltaTime = 0.1; // fixme : needs to be computed at each timestep with CFL condition
-        constexpr float maxTime = 600.;
-        constexpr float vehiclesPerMin = 30.;
+        const int nbDumps = 1000;
+        const float deltaTime = 0.1; // fixme : needs to be computed at each timestep with CFL condition
+        const float maxTime = 600.;
+        const float vehiclesPerMin = 15;
     }
 
     namespace car
     {
-        constexpr float brakingDistanceFactor = 2.5;
-        constexpr float cruiseDistanceFactor = 5.;
-        constexpr float accelDistanceFactor = 7.5;
-        constexpr Motion startMotion = Motion::Accelerating;
-        constexpr float startVelocityFactor = 0.5;
-        constexpr float safeDistanceToEnterLaneFactor = 2.;
+        const float brakingDistanceFactor = 2.5;
+        const float cruiseDistanceFactor = 5.;
+        const float accelDistanceFactor = 7.5;
+        const Motion startMotion = Motion::Accelerating;
+        const float startVelocityFactor = 0.5;
+        const float safeDistanceToEnterLaneFactor = 2.;
     }
 }
 
