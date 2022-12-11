b9e9c3dbce9704acd2264b01b555f4362a4b7e91
Arnaud Vericel <arnaud.vericel@gmail.com>
2022-12-11 17:02:36 +0100

    - Added an example of lane configuration through the config object in main

--- src/bottlenex.cpp
+++ src/bottlenex.cpp
@@ -6,15 +6,23 @@
 
 int main(int argc, char** argv)
 {
+    /* Run configuration */
     Config* myConfig = Config::GetConfig();
+
+    myConfig->AddLane(LaneData());
+    myConfig->AddLane(LaneData(4000, 110, 30, true, 1000, 1500, 90, 30));
+    myConfig->Set(Config::FloatSettings::MaxTimeMin, 5);
+
     myConfig->PrintConfig();
 
+    /* Instanciation of the Road object with its own vehicle factory */
     VehicleFactory* factory = new VehicleFactory();
+    Road* road = new Road(factory);
 
-    Road* road = new Road(1, factory);
-
+    /* Road evolution */
     road->Evolve();
 
+    /* End of the run */
     delete road;
     delete myConfig;
     delete factory;
